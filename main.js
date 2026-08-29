95a72c907fd38dd7dde345af10fdff2e7cc048e2
S-D-Nil <gamersclub3232@gmail.com>
2026-08-29 17:51:02 +0600

    fix scroll reavel  issue

--- main.js
+++ main.js
@@ -67,22 +67,23 @@ const sr = ScrollReveal({
 
   duration: 2000,
 
-  reset: true
+  reset: true,
+
 });
 
 sr.reveal(".home-title", {});
 
-sr.reveal(".home-scroll", { delay: 200 });
+sr.reveal(".home-scroll", { delay: 200});
 
-sr.reveal(".home-img", { origin: "right", delay: 400 });
+sr.reveal(".home-img", { origin: "right", delay: 300});
 
-sr.reveal(".about-img", { delay: 500 });
+sr.reveal(".about-img", { delay: 250 });
 
-sr.reveal(".about-subtitle", { delay: 300 });
+sr.reveal(".about-subtitle", { delay: 250 });
 
-sr.reveal(".about-profession", { delay: 400 });
+sr.reveal(".about-profession", { delay: 300 });
 
-sr.reveal(".about-text", { delay: 500 });
+sr.reveal(".about-text", { delay: 350 });
 
 sr.reveal(".about-social-icon", { delay: 300, interval: 200 });
 
@@ -90,18 +91,20 @@ sr.reveal(".skills-subtitle", {});
 
 sr.reveal(".skills-name", { distance: "20px", delay: 50, interval: 100 });
 
-sr.reveal(".skills-img", { delay: 400 });
+sr.reveal(".skills-img", { delay: 200 });
 
-sr.reveal(".webproject", { interval: 200 });
-sr.reveal(".portfolio-img", { interval: 200 });
+sr.reveal(".webproject", { interval: 200, duration: 3000 });
 
-sr.reveal(".contact-subtitle", {});
+sr.reveal(".portfolio-img", { interval: 200, delay: 200, origin: "bottom", reset: false,duration: 3000 });
 
-sr.reveal(".contact-text", { interval: 200 });
+sr.reveal(".contact-subtitle", {origin: "left", reset: false, delay: 400, duration: 3000 });
 
-sr.reveal(".contact-input", { delay: 400 });
+sr.reveal(".contact-text", { interval: 200, origin: "left", reset: false,delay: 400, duration: 3000 });
 
-sr.reveal(".contact-button", { delay: 600 });
+sr.reveal(".contact-input", { delay: 400, origin: "left", reset: false, duration: 3000 });
+
+sr.reveal(".contact-button", { delay: 500, origin: "right", reset: false, duration: 3000 });
+
 document.addEventListener("DOMContentLoaded", function () {
   alert(
     "Welcome to the personal Portfolio website of S.D.Nil. Feel free to DM."
